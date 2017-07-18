0abc17a5b82a9438e7e1c4bfebe8250e8ea3c736
Augustina Wear <awear@cnm.edu>
2017-07-18 13:36:45 -0600

    made index.php file

---
+++ index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Persona</title>
+	</head>
+	<body>
+		<p><strong>Persona</strong></p>
+		<p><strong>Name:</strong>Michell-Miller</p>
+		<h2><img class="Michelle-Miller-image" src="images/michelle miller .jpg" alt="Michelle"/></h2>
+		<p><strong>Age:</strong>42</p>
+
+		<p><strong>Profession:</strong></p>
+		<p>Michelle is a printmaker specializing in lithography, mixed media artist, she is a business owner. Michelle is represented in several galleries in US and Europe.
+			Michelle is also a teacher at a community college. She recently was granted of 5000.00 for Printmaking residency in Spain.
+		</p>
+
+		<p><strong>Technology:</strong></p>
+
+		<ul>
+			<li>Mac Book Pro user, 27-inch (diagonal) Retina 5K display,5120‑by‑2880 resolution, processor 18 core,memory 128GB,Storage 4TB SSD,
+				Graphics Radeon Pro Vega 64 graphics processor with 16GB of HBM2 memory, Video Support and Camera Four 4096‑by‑2304 (4K) external displays
+				at 60Hz,Operating System MacOS High Sierra. She also has a Wacom Intuos Art Pen and Touch Tablet for when she is in class or at the studio.
+				Michelle has an iPhone6S 5.5 128GB,44inch Epson Stylus Pro 9900,
+			</li>
+			<li> Michelle pays $129.90 a month for internet and which gives her internet access and phone.</li>
+		</ul>
+		<p><strong>Attitudes and Behaviors:</strong></p>
+		<ul>
+			<li>Due to the nature of Michelle businesses she has to be proficient with technology it part of her skill set as an artist she is classified as an
+				avid technology user.</li>
+						<li>Michelle needs her business to run efficiently, and needs to keep in contact with her assistant she while away, galleries need to be able stay in contact
+				with them as well.</li>
+		</ul>
+		<p><strong>Frustrations and Needs:</strong></p>
+		<ul>
+			<li>Michelle frustration is that she has so many things going on and needs a website to help: Organize, Send files and information to galleries and</li>
+			<li>Michelle schedule is very hectic, there is always a deadline she has to meet, worries work will arrive intact. Michelle is currently on sabbatical
+				preparing to go overseas for six plus week residency</li>
+			<li>Michelle needs her business to run efficiently, and needs to keep in contact with her assistant she while away, galleries need to be able stay in
+				contact with them while traveling</li>
+		</ul>
+		<p><strong>Goals:</strong>
+			Michelle's goal is to streamline her work flow and to get her work out there in the most effective cost efficient way.</p>
+
+		<p><strong>Use Story:</strong></p>
+		<p>As a user I want have a web site that will reflect me and my work</p>
+
+		<h2><strong>Use Case:</strong></h2>
+
+		<p>Due to her busy schedule life Michelle will use the website upload her works to prospective clients, galleries, work and network with other artists.</p>
+
+		<h2><strong>Interaction</strong></h2>
+		<ul>
+			<li>User has logged,web page uploads site, user than navigates to inventory listings,web page uploads listing, user opens excel and
+				website auto populates, prices, user enters new prices into cells, web page updates and loads new values.</li>
+			<li>User has logged in, web page acknowledges,user navigates to inventory updates several designs, web page saves and auto populates updated lists.</li>
+			<li>User has logged on, navigates to customer feedback,web page acknowledges and auto populates client list/comments,user navigates to custom orders tab,
+				finds and selects client,sends message,hits send, web page captures/sends.</li>
+			<li>User is logged on to site, navigates the shipping tab,web site acknowledges, auto populates shipping preferences, user picks one,
+				web site launches preference, user fills out drop down menus and hits send, web site and confirms with second verification and send confirmation back to user.</li>
+			<li>User has logged on, web site confirms,user navigates to up load page, inserts jeps,web site deploys them, user updates inventory jeps and confiirms,
+				website acknowledges and post new pictures of inventory on site</li>
+			<li>User logs on, web page acknowledges,sends alert,user reads and replies to buyer(s), web page then acknowledges and sends will need to buyer</li>
+		</ul>
+		<h2><strong>Conceptual</strong></h2>
+		<h3>Profile</h3>
+		<ul>
+			<li>profield(primary key)</li>
+			<li>profileActivationToken(for account verification</li>
+			<li>profileHandle</li>
+			<li>profileEmail<li>
+			<li>profileHash(for account password)</li>
+			<li>profilePhone<li>
+			<li>profileSalt(for account password)</li>
+		</ul>
+		<h3>Product</h3>
+		<p>User logged on,web acknowledges,user navigates to inventory tab,web page auto populates inventory materials,
+			user updates tab,web site updates of product.</p>
+
+</html>
