972a765a102205ea2ceb7baca4ea8d3408b8a981
Augustina Wear <awear@cnm.edu>
2017-07-20 17:01:57 -0600

    fixed path finished project

---
+++ public_html/Documentation/milestone-1.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<!-- meta charset here -->
+		<title>Persona</title>
+	</head>
+	<body>
+		<p><strong>Persona</strong></p>
+		<p><strong>Name:</strong>Michelle-Miller</p>
+		<h2><img class="Michelle-Miller-image" src="images/michelle miller .jpg" alt="Michelle"/></h2>
+		<p><strong>Age:</strong>42</p>
+
+		<p><strong>Profession:</strong></p>
+		<ul>
+			<li>Michelle is a printmaker specializing in lithography, mixed media artist, she is a business owner.</li>
+			<li>Michelle is represented by several galleries in US and Europe.</li>
+			<li>Michelle is also a teacher at a community college.</li>
+			<li>She recently was granted of 5000.00 for Printmaking residency in Spain.</li>
+		</ul>
+
+		<p><strong>Technology:</strong></p>
+
+		<ul>
+			<li>Mac Book Pro user, 27-inch (diagonal) Retina 5K display,5120‑by‑2880 resolution, processor 18 core,memory 128GB,Storage 4TB SSD,
+				Graphics Radeon Pro Vega 64 graphics processor with 16GB of HBM2 memory, Video Support and Camera Four 4096‑by‑2304 (4K) external displays
+				at 60Hz,Operating System MacOS High Sierra. She also has a Wacom Intuos Art Pen and Touch Tablet for when she is in class or at the studio.
+				Michelle has an iPhone6S 5.5 128GB,44inch Epson Stylus Pro 9900,
+			</li>
+		</ul>
+		<p><strong>Attitudes and Behaviors:</strong></p>
+		<ul>
+			<li>Due to the nature of Michelle businesses she has to be proficient with technology it part of her skill set as an artist she is classified as an
+				avid technology user. Michelle needs her business to run efficiently, and needs to keep in contact with her assistant she while away, galleries need to be able stay in contact
+				with With her busy schedule Michell</li>
+		</ul>
+		<p><strong>Frustrations and Needs:</strong></p>
+		<ul>
+			<li>Michelle frustration is that she has so many things going on and needs a website to help: Organize, Send files and information to galleries and</li>
+			<li>Michelle schedule is very hectic, there is always a deadline she has to meet, worries work will arrive intact. Michelle is currently on sabbatical
+				preparing to go overseas for six plus week residency</li>
+			<li>Michelle needs her business to run efficiently, and needs to keep in contact with her assistant she while away, galleries need to be able stay in
+				contact with them while traveling</li>
+		</ul>
+		<p><strong>Goals:</strong></p>
+		<p>Michelle's goal is to streamline her work flow and to get her work out there in the most effective cost efficient way.
+			She has gotten very busy in the past few months and has hired a assistant to help her while she is way on sabbatical.
+			Michelle needs a stonger eletronic presence, her business is getting very busy and she wants and needs a professional look on online</p>
+
+		<h2><strong>Use Story:</strong></h2>
+		<p>Michelle even though she is an avid user has little time. She hired a temporary assist wondering if she should hire her full time, she has gotten so busy.
+			Michelle had little to none online presence. She heard about questioner we give all prospective clients it help her friend
+			and thought it could point out what the companies needed and or up their production.</p>
+
+		<h2><strong>Use Case:</strong></h2>
+
+		<ul>
+			<li>Michelle heard of 2Gsolution through another artist whom we build a site for. Michelle friend called a and left me a message with her number. I called Michelle back and asked to set up a meeting.
+				We met at StarBucks and I gave Michelle the questioner that we give all prospective clients. Which identifies, needs, wants, how they a currently use computer/internet, for her business and also asked about Michelle's aesthetics
+				Taking the assessment Michelle realize that she 1. Didnt have a business social media platform it was non existent, 2. Daily operation could be streamed thru a web site to minimize her/assist work load.                                                                                                                                                                                                        upload her works to prospective clients, galleries, work and network with other artists.Michelle has little time but her
+				3.Find a assistant will be taking over those duties, 4.Increase productivity</li>
+		</ul>
+	</body>
+</html>
